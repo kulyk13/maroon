6d355df1b7ff62fd0a5e210604e3f89c89dc5f13
kulyk13 <13dimon1313@gmail.com>
2021-12-16 15:03:20 +0200

    create bestsellers section

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Slider
+const bestSlider = new Swiper('.bestsellers-slider', {
+    loop: true,
+    speed: 400,
+    spaceBetween: 30,
+    slidesPerView: 4,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
